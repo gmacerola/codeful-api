866aee4cc4b66673925a75abd6ef571f8965a752
Gianpaolo Macerola <gmacerola@gmail.com>
2020-11-30 16:01:27 -0600

    working but needs spec with users

--- src/middleware/jwt-auth.js
+++ src/middleware/jwt-auth.js
@@ -4,11 +4,11 @@ function requireAuth(req, res, next) {
   const authToken = req.get("Authorization") || "";
 
   let bearerToken;
-  if (!authToken.toLowerCase().startsWith("bearer ")) {
-    return res.status(401).json({ error: "Missing bearer token" });
-  } else {
-    bearerToken = authToken.slice(7, authToken.length);
-  }
+  //   if (!authToken.toLowerCase().startsWith("bearer ")) {
+  //     return res.status(401).json({ error: "Missing bearer token" });
+  //   } else {
+  bearerToken = authToken.slice(7, authToken.length);
+  //   }
 
   try {
     const payload = AuthService.verifyJwt(bearerToken);
