1fe3a4675696e022b18a658cd0537ad497ff4da9
Gianpaolo Macerola <gmacerola@gmail.com>
2020-12-02 13:55:42 -0600

    removed client origin from cors

--- src/app.js
+++ src/app.js
@@ -13,11 +13,7 @@ const authRouter = require("./auth/auth-router");
 const app = express();
 
 const morganOption = NODE_ENV === "production" ? "tiny" : "common";
-app.use(
-  cors({
-    origin: CLIENT_ORIGIN,
-  })
-);
+app.use(cors());
 
 app.use(morgan(morganOption));
 app.use(helmet());
