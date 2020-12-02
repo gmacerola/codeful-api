aff386728abe754bae2a36296992bc8e3f177adc
Gianpaolo Macerola <gmacerola@gmail.com>
2020-12-02 08:52:26 -0600

    changed client origin to vercel

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || "development",
-  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || "http://localhost:3000/",
+  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || "https://codeful.vercel.app/",
   DATABASE_URL:
     process.env.DATABASE_URL || "postgresql://postgres@localhost/codeful",
   JWT_SECRET: process.env.JWT_SECRET || "codeful-jwt",
